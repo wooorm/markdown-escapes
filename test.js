d952a1d77598413ec302ac655b5ecc6bd934448a
Titus Wormer <tituswormer@gmail.com>
2018-04-18 19:06:10 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,34 +1,36 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var escapes = require('.');
+var test = require('tape')
+var escapes = require('.')
 
-test('escapes()', function (t) {
-  t.ok(escapes.default.every(function (escape) {
-    return escapes.gfm.indexOf(escape) !== -1;
-  }), 'all default escapes should be in gfm');
+test('escapes()', function(t) {
+  t.ok(
+    escapes.default.every(function(escape) {
+      return escapes.gfm.indexOf(escape) !== -1
+    }),
+    'all default escapes should be in gfm'
+  )
 
-  t.ok(escapes.gfm.every(function (escape) {
-    return escapes.commonmark.indexOf(escape) !== -1;
-  }), 'all gfm escapes should be in commonmark');
+  t.ok(
+    escapes.gfm.every(function(escape) {
+      return escapes.commonmark.indexOf(escape) !== -1
+    }),
+    'all gfm escapes should be in commonmark'
+  )
 
   t.equal(
     escapes({commonmark: true}),
     escapes.commonmark,
     'should return commonmark escapes if `commonmark: true`'
-  );
+  )
 
   t.equal(
     escapes({gfm: true}),
     escapes.gfm,
     'should return gfm escapes if `gfm: true`'
-  );
+  )
 
-  t.equal(
-    escapes(),
-    escapes.default,
-    'should return default escapes otherwise'
-  );
+  t.equal(escapes(), escapes.default, 'should return default escapes otherwise')
 
-  t.end();
-});
+  t.end()
+})
